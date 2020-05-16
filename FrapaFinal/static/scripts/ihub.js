5a10a9c2d9a4b09961d83e8a0553d479a5d1a328
rajkartha <54449861+rajkartha@users.noreply.github.com>
2020-05-16 07:22:11 -0400

    fixed ihub

--- FrapaFinal/static/scripts/ihub.js
+++ FrapaFinal/static/scripts/ihub.js
@@ -1,70 +1,70 @@
 function init() {
-  // Grab a reference to the dropdown select element
-  var departments = d3.select("#Departments");
-  var aisles = d3.select("#Aisles");
-  var products = d3.select("#Products");
-
-  // Use the list of sample names to populate the select options
-  d3.json("Departments.json").then((data) => {
-
-    data.forEach((department) => {
-      departments
-        .append("option")
-        .text(department.Antecedents)
-        .property("value", department.Antecedents);
-    });
-  })
-   // Use the list of sample names to populate the select options
-   d3.json("Aisles.json").then((data) => {
-
-    data.forEach((aisle) => {
-      aisles
-        .append("option")
-        .text(aisle.Antecedents)
-        .property("value", aisle.Antecedents);
-    });
-  })
-   // Use the list of sample names to populate the select options
-   d3.json("Products.json").then((data) => {
-
-    data.forEach((product) => {
-      products
-        .append("option")
-        .text(product.Antecedents)
-        .property("value", product.Antecedents);
-    });
-  })
-
-}
-
-function optionChanged(selection, output) {
-  var consequent = d3.select('#'+ output)
-  if(output == "Department_C") {
-    var data_file = "Departments.json"
-  } else if (output == "Aisle_C") {
-    var data_file = "Aisles.json"
-  } else {
-    var data_file = "Products.json"
+    // Grab a reference to the dropdown select element
+    var departments = d3.select("#Departments");
+    var aisles = d3.select("#Aisles");
+    var products = d3.select("#Products");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("/static/Departments.json").then((data) => {
+  
+      data.forEach((department) => {
+        departments
+          .append("option")
+          .text(department.Antecedents)
+          .property("value", department.Antecedents);
+      });
+    })
+     // Use the list of sample names to populate the select options
+     d3.json("/static/Aisles.json").then((data) => {
+  
+      data.forEach((aisle) => {
+        aisles
+          .append("option")
+          .text(aisle.Antecedents)
+          .property("value", aisle.Antecedents);
+      });
+    })
+     // Use the list of sample names to populate the select options
+     d3.json("/static/Products.json").then((data) => {
+  
+      data.forEach((product) => {
+        products
+          .append("option")
+          .text(product.Antecedents)
+          .property("value", product.Antecedents);
+      });
+    })
+  
   }
-
-
-
-  d3.json(data_file).then((data) => {
-    var filtered_data = data.filter(obj => obj.Antecedents == selection)
-    filtered_data.forEach((departments) => {
-      consequent
-        .html(null)
-        .append("p")
-        .text(departments.Consequents);
-    });
-
-
-  })
-
-
-}
-
-// Initialize the dashboard
-init();
-
-
+  
+  function optionChanged(selection, output) {
+    var consequent = d3.select('#'+ output)
+    if(output == "Department_C") {
+      var data_file = "Departments.json"
+    } else if (output == "Aisle_C") {
+      var data_file = "Aisles.json"
+    } else {
+      var data_file = "Products.json"
+    }
+  
+  
+  
+    d3.json("/static/"+ data_file).then((data) => {
+      var filtered_data = data.filter(obj => obj.Antecedents == selection)
+      filtered_data.forEach((departments) => {
+        consequent
+          .html(null)
+          .append("p")
+          .text(departments.Consequents);
+      });
+  
+  
+    })
+  
+  
+  }
+  
+  // Initialize the dashboard
+  init();
+  
+  
